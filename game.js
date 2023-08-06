affb867becece37b8d3c7d98dbb13d0837870b49
JordanVermeire <emotionaltrashpanda@gmail.com>
2023-08-06 00:58:41 -0500

    Fixed paddle glitch so the ball does not go through them or move them Added a ball reset Updated player score to count up Added a "winner" statement when a player reaches 5

--- game.js
+++ game.js
@@ -4,16 +4,17 @@ const WINDOW_HEIGHT = 600;
 const SEP_RATIO = 8;
 
 
-var player1, player2;
-var cursors;
-var ball;
-var randomBallVelocity = 0;
+let player1, player2;
+let cursors;
+let ball;
+let randomBallVelocity = 0;
 
-var p1Score = 0;
-var p2Score = 0;
-var scoreText;
+let p1Score = 0;
+let p2Score = 0;
+let winText;
+let scoreText;
 
-var config = {
+const config = {
     type: Phaser.AUTO,
     width: WINDOW_WIDTH,
     height: WINDOW_HEIGHT,
@@ -31,7 +32,23 @@ var config = {
     }
 };
 
-var game = new Phaser.Game(config);
+function ResetBall() {
+    ball.setPosition(WINDOW_WIDTH/2, WINDOW_HEIGHT/2);
+    let randomNumForDirection = Phaser.Math.Between(1,100);
+    if(randomNumForDirection > 50) {
+        ball.setVelocityX(Phaser.Math.Between(200,250));
+        ball.setVelocityY(Phaser.Math.Between(150,250));
+    } else {
+        ball.setVelocityX(Phaser.Math.Between(-250,-200));
+        ball.setVelocityY(Phaser.Math.Between(-250,-150));
+    }
+}
+
+function DeflectBall() {
+    ball.setVelocityX(-ball.body.velocity.x);
+}
+
+const game = new Phaser.Game(config);
 
 function preload (){
     this.load.image('sky', './resc/img/cloudyskypong.png');
@@ -46,20 +63,22 @@ function preload (){
 function create(){
     this.add.image(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, 'sky');
 
-    player1 = this.physics.add.sprite(WINDOW_WIDTH/SEP_RATIO, WINDOW_HEIGHT/2, 'paddle1');
-    player2 = this.physics.add.sprite((WINDOW_WIDTH/SEP_RATIO)*(SEP_RATIO-1), WINDOW_HEIGHT/2, 'paddle2');
+    player1 = this.physics.add.sprite(WINDOW_WIDTH/SEP_RATIO, WINDOW_HEIGHT/2, 'paddle1').setImmovable();
+    player2 = this.physics.add.sprite((WINDOW_WIDTH/SEP_RATIO)*(SEP_RATIO-1), WINDOW_HEIGHT/2, 'paddle2').setImmovable();;
     player1.setCollideWorldBounds(true);
     player2.setCollideWorldBounds(true);
-    this.physics.add.collider(player1, player2);
+
 
     ball = this.physics.add.sprite(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, 'ball');
     ball.setCollideWorldBounds(true);
     ball.setBounce(1,1);
+    ball.setCircle(32, 0, 0);
     this.physics.add.collider(player1, ball);
     this.physics.add.collider(player2, ball);
-
-    ball.setVelocityX(Phaser.Math.Between(-200, 200));
-    ball.setVelocityY(Phaser.Math.Between(-200,200));
+    //this.physics.add.collider(player1, ball, DeflectBall);
+    //this.physics.add.collider(player2, ball, DeflectBall);
+    
+    ResetBall();
 
     //UI
     scoreText = this.add.text(WINDOW_WIDTH/2-50, 16, p1Score + ' - ' + p2Score, {fontSize: '44px' , fill: '#fff'});
@@ -68,9 +87,7 @@ function create(){
     rKey.on('down' , (key, event) =>
     {   
         event.stopPropagation();
-        ball.setPosition(WINDOW_WIDTH/2, WINDOW_HEIGHT/2);
-        ball.setVelocityX(Phaser.Math.Between(-200, 200));
-        ball.setVelocityY(Phaser.MAth.Between(-200,200));
+        ResetBall();
     });
 
     const wKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
@@ -87,6 +104,7 @@ function create(){
         player1.setVelocityY(360);
     });
 
+
     const upKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
     upKey.on('down', (key, event) =>
     {
@@ -102,6 +120,47 @@ function create(){
     });
 }
 
-function update (){
+function checkBallPositionForScore(position) {
+    if (position.x < 10){
+        p2Score++;
+        ResetBall();
+    } else if (position.x > WINDOW_WIDTH-70) {
+        p1Score++;
+        ResetBall();
+    }
+}
 
+function ResetPlayerPositions() {
+    player1.setPosition(WINDOW_WIDTH/SEP_RATIO, WINDOW_HEIGHT/2);
+    player2.setPosition((WINDOW_WIDTH/SEP_RATIO)*(SEP_RATIO-1), WINDOW_HEIGHT/2);
+}
+
+function ResetGame() {
+    p1Score = 0;
+    p2Score = 0;
+    winText.destroy();
+    ResetPlayerPositions();
+    ResetBall();
+}
+
+function update (){
+    player1.setVelocityX(0);
+    player2.setVelocityX(0);
+
+    
+    checkBallPositionForScore(ball.body.position);
+
+    if(p1Score >= 5 || p2Score >= 5) {
+        if (p1Score > p2Score) {
+        this.add.text(150, WINDOW_HEIGHT/2-50, 'Player 1 Wins!' , { fontSize: '60px' , fill: '#fff'});
+    } else {
+        this.add.text(150, WINDOW_HEIGHT/2-50, 'Player 2 Wins!' , { fontSize: '60px' , fill: '#fff'});
+    }
+    this.input.keyboard.on('keydown' , (event) => {
+        if (event.keyCode === 32) {
+            ResetGame();
+        }
+    });
+    }
+    scoreText.setText(p1Score + ' - ' + p2Score);
 }
